1f9596d0526d83c663496b3aef60c5de6bcc5abc
ayuariesta <=>
2022-11-16 17:34:01 +0700

    beban pengukuran

--- lib/screen/beban.dart
+++ lib/screen/beban.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mocoringan_app/constants.dart';
+import 'input_beban.dart';
 
 class DataBebanList extends StatefulWidget {
   const DataBebanList({super.key});
@@ -8,8 +11,196 @@ class DataBebanList extends StatefulWidget {
 }
 
 class _DataBebanListState extends State<DataBebanList> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  final CollectionReference _bebans =
+      FirebaseFirestore.instance.collection('beban');
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    double widthScreen = mediaQueryData.size.width;
+    double heightScreen = mediaQueryData.size.height;
+
+    return Scaffold(
+      key: _scaffoldKey,
+      backgroundColor: kPrimaryLightColor,
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            _buildWidgetBebanList(widthScreen, heightScreen, context),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          color: kPrimaryLightColor,
+        ),
+        onPressed: () async {
+          bool? result = await Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => BebanForm(isEdit: false)));
+          if (result != null && result) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text('Data Beban Berhasil Ditambahkan'),
+              duration: Duration(seconds: 3),
+            ));
+            setState(() {});
+          }
+        },
+        backgroundColor: kPrimaryColor,
+      ),
+    );
+  }
+
+  Container _buildWidgetBebanList(
+      double widthScreen, double heightScreen, BuildContext context) {
+    return Container(
+      width: widthScreen,
+      height: heightScreen,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, top: 16.0),
+            child: Text(
+              'Data Beban',
+              style: TextStyle(
+                  color: kPrimaryColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+          ),
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: firebaseFirestore
+                  .collection('beban')
+                  .orderBy('date')
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                return ListView.builder(
+                  padding: EdgeInsets.all(8.0),
+                  itemCount: snapshot.data?.docs.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    DocumentSnapshot documentSnapshot =
+                        snapshot.data!.docs[index];
+                    Map<String, dynamic>? protek =
+                        documentSnapshot.data() as Map<String, dynamic>?;
+                    String strDate = protek!['date'];
+                    return Card(
+                      child: ListTile(
+                        title: Text(protek['section']),
+                        subtitle: Text(
+                          protek['arus'],
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        isThreeLine: false,
+                        leading: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                              width: 24.0,
+                              height: 24.0,
+                              decoration: BoxDecoration(
+                                color: kPrimaryLightColor,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Center(
+                                child: Text(
+                                  '${int.parse(strDate.split(' ')[0])}',
+                                  style: TextStyle(color: kPrimaryColor),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 4.0),
+                            Text(
+                              strDate.split(' ')[1],
+                              style: TextStyle(fontSize: 12.0),
+                            ),
+                          ],
+                        ),
+                        trailing: PopupMenuButton(
+                          itemBuilder: (BuildContext context) {
+                            // ignore: deprecated_member_use
+                            return <PopupMenuEntry<String>>[]
+                              ..add(PopupMenuItem<String>(
+                                value: 'edit',
+                                child: Text('Edit'),
+                              ))
+                              ..add(PopupMenuItem<String>(
+                                value: 'delete',
+                                child: Text('Delete'),
+                              ));
+                          },
+                          onSelected: (String value) async {
+                            if (value == 'edit') {
+                              // TODO: fitur edit task
+                              bool? result = await Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) {
+                                  return BebanForm(
+                                    isEdit: true,
+                                    bebanId: documentSnapshot.id,
+                                    namaSection: protek['section'],
+                                    nilaiUkur: protek['ukur'],
+                                    tanggal: protek['date'],
+                                  );
+                                }),
+                              );
+                              if (result != null && result) {
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(SnackBar(
+                                  content: Text('Data Beban Berhasil Diupdate'),
+                                ));
+                                setState(() {});
+                              }
+                            } else if (value == 'delete') {
+                              // TODO: fitur hapus task
+                              showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: Text('Apakah kamu yakin?'),
+                                      content: Text(
+                                          'Apakah kamu ingin menghapus ${protek['section']}'),
+                                      actions: <Widget>[
+                                        TextButton(
+                                          child: Text('No'),
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                        ),
+                                        TextButton(
+                                          child: Text('Delete'),
+                                          onPressed: () {
+                                            documentSnapshot.reference.delete();
+                                            Navigator.pop(context);
+                                            setState(() {});
+                                          },
+                                        ),
+                                      ],
+                                    );
+                                  });
+                            }
+                          },
+                          child: Icon(Icons.more_vert),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/screen/input_beban.dart
@@ -0,0 +1,244 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:mocoringan_app/constants.dart';
+import 'package:intl/intl.dart';
+
+class BebanForm extends StatefulWidget {
+  final bool isEdit;
+  final String bebanId;
+  final String namaSection;
+  final String tanggal;
+  final String nilaiUkur;
+
+  const BebanForm({
+    required this.isEdit,
+    this.bebanId = '',
+    this.namaSection = '',
+    this.tanggal = '',
+    this.nilaiUkur = '',
+  });
+
+  @override
+  State<BebanForm> createState() => _BebanFormState();
+}
+
+class _BebanFormState extends State<BebanForm> {
+  var bebanID = "";
+
+  GlobalKey<ScaffoldState> _scaffoldState = GlobalKey();
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  TextEditingController sectionController = TextEditingController();
+  TextEditingController tanggalController = TextEditingController();
+  TextEditingController ukurController = TextEditingController();
+
+  late double widthScreen;
+  late double heightScreen;
+  DateTime tanggal = DateTime.now().add(Duration(days: 1));
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    if (widget.isEdit) {
+      tanggal = DateFormat('dd MMMM yyyy').parse(widget.tanggal);
+      sectionController.text = widget.namaSection;
+      tanggalController.text = widget.tanggal;
+      ukurController.text = widget.nilaiUkur;
+    } else {
+      tanggalController.text = DateFormat('dd MMMM yyyy').format(tanggal);
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    widthScreen = mediaQueryData.size.width;
+    heightScreen = mediaQueryData.size.height;
+
+    return Scaffold(
+      key: _scaffoldState,
+      backgroundColor: kPrimaryLightColor,
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              width: widthScreen,
+              height: heightScreen,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  _buildWidgetFormPrimary(),
+                  SizedBox(height: 16.0),
+                  _buildWidgetFormSecondary(),
+                  isLoading
+                      ? Container(
+                          color: Colors.white,
+                          padding: const EdgeInsets.all(16.0),
+                          child: Center(
+                            child: CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation<Color>(
+                                  kPrimaryLightColor),
+                            ),
+                          ),
+                        )
+                      : _buildWidgetButtonCreateBeban(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWidgetFormPrimary() {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(
+              Icons.arrow_back,
+              color: Colors.grey[800],
+            ),
+          ),
+          SizedBox(height: 16.0),
+          Text(
+            widget.isEdit ? 'Edit\nData Beban' : 'Create\nData Beban',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.grey[800],
+              fontSize: 25.0,
+            ),
+          ),
+          SizedBox(height: 16.0),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildWidgetFormSecondary() {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(24.0),
+            topRight: Radius.circular(24.0),
+          ),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: sectionController,
+              decoration: InputDecoration(
+                labelText: 'Nama Section',
+              ),
+              style: TextStyle(fontSize: 18.0),
+            ),
+            TextField(
+              controller: ukurController,
+              decoration: InputDecoration(
+                label: Text('Nilai Pengukuran'),
+                suffixIcon: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Icon(Icons.electric_bolt),
+                  ],
+                ),
+              ),
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: tanggalController,
+              decoration: InputDecoration(
+                label: Text('Tanggal'),
+                suffixIcon: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Icon(Icons.today),
+                  ],
+                ),
+              ),
+              style: TextStyle(fontSize: 18.0),
+              readOnly: true,
+              onTap: () async {
+                DateTime today = DateTime.now();
+                DateTime? datePicker = await showDatePicker(
+                  context: context,
+                  initialDate: tanggal,
+                  firstDate: today,
+                  lastDate: DateTime(2090),
+                );
+                if (datePicker != null) {
+                  tanggal = datePicker;
+                  tanggalController.text =
+                      DateFormat('dd MMMM yyyy').format(tanggal);
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWidgetButtonCreateBeban() {
+    return Container(
+      width: double.infinity,
+      color: Colors.white,
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: ElevatedButton(
+        child: Text(widget.isEdit ? 'UPDATE DATA' : 'SIMPAN'),
+        onPressed: () async {
+          String section = sectionController.text;
+          String ukur = ukurController.text;
+          String date = tanggalController.text;
+          setState(() => isLoading = true);
+          if (widget.isEdit) {
+            DocumentReference documentReference =
+                firebaseFirestore.doc('beban/${widget.bebanId}');
+            firebaseFirestore.runTransaction((transaction) async {
+              DocumentSnapshot protek =
+                  await transaction.get(documentReference);
+              if (protek.exists) {
+                await transaction.update(
+                  documentReference,
+                  <String, dynamic>{
+                    'section': section,
+                    'ukur': ukur,
+                    'date': date,
+                  },
+                );
+                Navigator.pop(context, true);
+              }
+            });
+          } else {
+            CollectionReference beban = firebaseFirestore.collection('beban');
+            DocumentReference result = await beban.add(<String, dynamic>{
+              'section': section,
+              'ukur': ukur,
+              'date': date,
+            });
+            if (result.id != null) {
+              Navigator.pop(context, true);
+            }
+          }
+        },
+      ),
+    );
+  }
+
+  void _showSnackBarMessage(String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+}
