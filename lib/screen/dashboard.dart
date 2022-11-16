d75f4916e4821ce0fa63a564499f82cb23fb87c5
ayuariesta <=>
2022-11-17 05:52:50 +0700

    halaman dashboard

--- lib/screen/dashboard.dart
+++ lib/screen/dashboard.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mocoringan_app/constants.dart';
+import 'search_data.dart';
+import 'search_beban.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'Login/login_screen.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
@@ -9,38 +13,203 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  var idUser = "";
+  var nama = "";
+  var username = "";
+
+  @override
+  void initState() {
+    userCek();
+    super.initState();
+  }
+
+  userCek() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    var id = prefs.getString('id');
+    var name = prefs.getString('nama');
+    var usern = prefs.getString('username');
+
+    if (id == null) {
+      Navigator.pop(context);
+    } else {
+      setState(() {
+        idUser = id;
+        nama = name!;
+        username = usern!;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: kPrimaryColor,
-        title: const Text('Mocoringan'),
+        title: const Text('Mocoringan App'),
       ),
-      body: GridView.count(
-        padding: const EdgeInsets.all(25),
-        crossAxisCount: 2,
-        children: <Widget>[
-          Card(
-            margin: const EdgeInsets.all(8),
-            child: InkWell(
-              onTap: () {},
-              splashColor: kPrimaryColor,
-              child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: const <Widget>[
-                    Icon(
-                      Icons.home,
-                      size: 70,
-                      color: kPrimaryColor,
-                    ),
-                    Text("Input Data", style: TextStyle(fontSize: 17.0)),
-                  ],
-                ),
+      body: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Welcome,",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: kPrimaryColor,
               ),
             ),
-          ),
-        ],
+            Text(
+              nama,
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+                color: kPrimaryColor,
+              ),
+            ),
+            GridView.count(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(25),
+              crossAxisCount: 2,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SearchData()),
+                    );
+                  },
+                  child: Card(
+                    margin: const EdgeInsets.all(8),
+                    elevation: 4,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SearchData()),
+                        );
+                      },
+                      splashColor: kPrimaryColor,
+                      child: Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: const <Widget>[
+                            Icon(
+                              Icons.search,
+                              size: 70,
+                              color: kPrimaryColor,
+                            ),
+                            Text("Cari\nData Proteksi",
+                                style: TextStyle(fontSize: 17.0)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SearchBeban()),
+                    );
+                  },
+                  child: Card(
+                    margin: const EdgeInsets.all(8),
+                    elevation: 4,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SearchBeban()),
+                        );
+                      },
+                      splashColor: kPrimaryColor,
+                      child: Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: const <Widget>[
+                            Icon(
+                              Icons.search_outlined,
+                              size: 70,
+                              color: kPrimaryColor,
+                            ),
+                            Text("Cari\nData Beban",
+                                style: TextStyle(fontSize: 17.0)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () async {
+                    final pref = await SharedPreferences.getInstance();
+                    pref.clear(); // <- remove sharedpreferences
+
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                new LoginScreen()));
+                  },
+                  child: Card(
+                    margin: const EdgeInsets.all(8),
+                    elevation: 4,
+                    child: InkWell(
+                      onTap: () async {
+                        final pref = await SharedPreferences.getInstance();
+                        pref.clear(); // <- remove sharedpreferences
+
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    new LoginScreen()));
+                      },
+                      splashColor: kPrimaryColor,
+                      child: Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: const <Widget>[
+                            Icon(
+                              Icons.logout,
+                              size: 70,
+                              color: kPrimaryColor,
+                            ),
+                            Text("Logout", style: TextStyle(fontSize: 17.0)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWidgetButtonLogout() {
+    return Container(
+      color: Colors.white,
+      child: ElevatedButton(
+        child: Text('LOGOUT'),
+        onPressed: () async {
+          final pref = await SharedPreferences.getInstance();
+          pref.clear(); // <- remove sharedpreferences
+
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) => new LoginScreen()));
+        },
       ),
     );
   }
